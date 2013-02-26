e040d9c1bfd5ee926657b300ffef64f621367acc
Josef Lange <josef.d.lange@me.com>
2013-02-26 14:45:25 -0800

    Added RestKit to project and project configuration. Everything builds appropriately. RestKit is a git submodule here, which is pretty rad.

---
+++ iOS/Expresso/Expresso/EXNetworkConnectionController.h
@@ -0,0 +1,16 @@
+//
+//  EXNetworkConnectionController.h
+//  Expresso
+//
+//  Created by Josef Lange on 2/26/13.
+//  Copyright (c) 2013 Josef Lange & Daniel Guilak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <RestKit/RestKit.h>
+
+@interface EXNetworkConnectionController : NSObject
+
+
+
+@end
---
+++ iOS/Expresso/Expresso/EXNetworkConnectionController.m
@@ -0,0 +1,13 @@
+//
+//  EXNetworkConnectionController.m
+//  Expresso
+//
+//  Created by Josef Lange on 2/26/13.
+//  Copyright (c) 2013 Josef Lange & Daniel Guilak. All rights reserved.
+//
+
+#import "EXNetworkConnectionController.h"
+
+@implementation EXNetworkConnectionController
+
+@end
