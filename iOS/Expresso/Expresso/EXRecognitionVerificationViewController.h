fd59af107b99973c74ba3efb751da23c969d032a
Josef Lange <josef.d.lange@me.com>
2013-02-27 12:25:09 -0800

    Added ViewController for recognition verification view..?

---
+++ iOS/Expresso/Expresso/EXRecognitionVerificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  EXRecognitionVerificationViewController.h
+//  Expresso
+//
+//  Created by Josef Lange on 2/27/13.
+//  Copyright (c) 2013 Josef Lange & Daniel Guilak. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface EXRecognitionVerificationViewController : NSViewController
+
+@end
---
+++ iOS/Expresso/Expresso/EXRecognitionVerificationViewController.m
@@ -0,0 +1,13 @@
+//
+//  EXRecognitionVerificationViewController.m
+//  Expresso
+//
+//  Created by Josef Lange on 2/27/13.
+//  Copyright (c) 2013 Josef Lange & Daniel Guilak. All rights reserved.
+//
+
+#import "EXRecognitionVerificationViewController.h"
+
+@implementation EXRecognitionVerificationViewController
+
+@end
