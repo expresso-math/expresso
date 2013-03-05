3a72e82ee2026aa747cde07a2c512fa35e44b274
Josef Lange <josef.d.lange@me.com>
2013-03-05 13:01:22 -0800

    Removed RestKit submodule.

---
+++ iOS/Expresso/Expresso/EXIdleConnectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  EXIdleConnectionViewController.h
+//  Expresso
+//
+//  Created by Josef Lange on 3/5/13.
+//  Copyright (c) 2013 Josef Lange & Daniel Guilak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EXIdleConnectionViewController : UIViewController
+
+@end
---
+++ iOS/Expresso/Expresso/EXIdleConnectionViewController.m
@@ -0,0 +1,38 @@
+//
+//  EXIdleConnectionViewController.m
+//  Expresso
+//
+//  Created by Josef Lange on 3/5/13.
+//  Copyright (c) 2013 Josef Lange & Daniel Guilak. All rights reserved.
+//
+
+#import "EXIdleConnectionViewController.h"
+
+@interface EXIdleConnectionViewController ()
+
+@end
+
+@implementation EXIdleConnectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
